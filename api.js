5cf98a0f3ad99a91c8c0eb674b49d4374823a224
leen2o <cnfoxy@gmail.com>
2025-09-06 18:11:22 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,8 @@
 // 精选内容 (首页和精选页面) - 更新API接口
-{ url: 'https://new.xianbao.fun/plus/json/push.json', type: 'xianbao', name: '线报网', list:'bmd' ,int:"10"},
-{ url: 'https://new.xianbao.fun/plus/json/push_19.json', type: 'xianbao', name: '线报网', list:'bmd' ,int:"10"},
-{ url: 'https://new.xianbao.fun/plus/json/push_11.json', type: 'xianbao', name: '线报网', list:'bmd' ,int:"10"},
+{ url: 'https://new.xianbao.fun/plus/json/push.json', type: 'xianbao', name: '线报网', list:'bmd' ,int:"30"},
+{ url: 'https://new.xianbao.fun/plus/json/push_11.json', type: 'xianbao', name: '线报网', list:'bmd' ,int:"30"},
+{ url: 'https://new.xianbao.fun/plus/json/push_16.json', type: 'xianbao', name: '线报网', list:'bmd' ,int:"30"},
+{ url: 'https://new.xianbao.fun/plus/json/push_18.json', type: 'xianbao', name: '线报网', list:'bmd' ,int:"30"},
+{ url: 'https://new.xianbao.fun/plus/json/push_19.json', type: 'xianbao', name: '线报网', list:'bmd' ,int:"30"},
 { url: 'https://faxian.smzdm.com/json_more?type=a&timesort=1799999999', type: 'hot', name: '什么值得买' , list:'bmd',int:"300"},
 //{ url: 'https://www.smzdm.com/jingxuan/json_more?timesort=1759999999&filter=s0f0t0b0d0r0p0', type: 'hot', name: '什么值得买' , list:'bmd'},
