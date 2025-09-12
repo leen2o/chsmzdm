563fcbdc22f6fdf164fba693faf8470f8d13ffc1
leen2o <cnfoxy@gmail.com>
2025-09-12 14:13:56 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 // 精选内容 (首页和精选页面) - 更新API接口
-{ url: 'https://new.xianbao.fun/plus/json/push.json', type: 'xianbao', name: '线报网', list:'bmd' ,int:"100"},
-{ url: 'https://new.xianbao.fun/plus/json/push_10.json', type: 'xianbao', name: '线报网', list:'bmd' ,int:"120"},
+//{ url: 'https://new.xianbao.fun/plus/json/push.json', type: 'xianbao', name: '线报网', list:'bmd' ,int:"100"},
+//{ url: 'https://new.xianbao.fun/plus/json/push_10.json', type: 'xianbao', name: '线报网', list:'bmd' ,int:"120"},
 { url: 'https://new.xianbao.fun/plus/json/push_11.json', type: 'xianbao', name: '线报网', list:'bmd' ,int:"140"},
 { url: 'https://new.xianbao.fun/plus/json/push_16.json', type: 'xianbao', name: '线报网', list:'bmd' ,int:"160"},
 { url: 'https://new.xianbao.fun/plus/json/push_18.json', type: 'xianbao', name: '线报网', list:'bmd' ,int:"180"},
