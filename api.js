72f8df8b2a09390a1422b06635c9de999be78064
leen2o <cnfoxy@gmail.com>
2025-09-24 15:46:27 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 { url: 'https://www.hxm5.com/s/bug%E4%BB%B7/', type: 'web', xp_t: '//li[@class="card"]/a/h2',xp_u: '//li[@class="card"]/a/@href',pre_u: '',name: '线报屋-bug价(XPath)', list: 'hmd',int: "220"},
 { url: 'https://www.hxm5.com/s/bug%E4%BB%B7/', type: 'web', reg_t: '<h2>(.*?)</h2>',reg_u: 'href="([^"]*)"',pre_u: 'https://www.hxm5.com',name: '线报屋-bug价(Regex)', list: 'hmd',int: "220"},
-{ url: 'https://new.xianbao.fun/plus/json/push.json', type: 'xianbao', name: '线报网-全部', list:'bmd' ,int:"100"}, 
+{ url: 'https://new.xianbao.fun/plus/json/push.json', type: 'xianbao', name: '线报网-全部', list:'bmd' ,int:"100"},  
 { url: 'https://new.xianbao.fun/plus/json/push_10.json', type: 'xianbao', name: '线报网-微博线报', list:'bmd' ,int:"120"},
 { url: 'https://new.xianbao.fun/plus/json/push_11.json', type: 'xianbao', name: '线报网-小嘀咕', list:'bmd' ,int:"140"},
 { url: 'https://new.xianbao.fun/plus/json/push_16.json', type: 'xianbao', name: '线报网-赚客吧', list:'bmd' ,int:"160"},
