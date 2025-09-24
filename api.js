31f7030f431e32240650f6a8f8cb7c4a0b0e5dd8
leen2o <cnfoxy@gmail.com>
2025-09-24 16:11:37 +0800

    Update api.js

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
-{ url: 'https://www.hxm5.com/s/%E5%B0%8F%E7%B1%B317/', type: 'web', xp_t: '//li[@class="card"]/a/h2',xp_u: '//li[@class="card"]/a/@href',pre_u: 'https://www.hxm5.com',name: '线报屋-速度', list: 'hmd',int: "220"},
-{ url: 'https://www.hxm5.com/s/iphone%2017/', type: 'web', xp_t: '//li[@class="card"]/a/h2',xp_u: '//li[@class="card"]/a/@href',pre_u: 'https://www.hxm5.com',name: '线报屋-速度', list: 'hmd',int: "220"},
+{ url: 'https://www.hxm5.com/s/%E5%B0%8F%E7%B1%B317/', type: 'web', xp_t: '//li[@class="card"]/a/h2',xp_u: '//li[@class="card"]/a/@href',pre_u: 'https://www.hxm5.com',name: '线报屋-小米17', list: 'hmd',int: "220"},
+{ url: 'https://www.hxm5.com/s/iphone%2017/', type: 'web', xp_t: '//li[@class="card"]/a/h2',xp_u: '//li[@class="card"]/a/@href',pre_u: 'https://www.hxm5.com',name: '线报屋-iphone17', list: 'hmd',int: "220"},
 { url: 'https://www.hxm5.com/s/%E9%80%9F%E5%BA%A6/', type: 'web', xp_t: '//li[@class="card"]/a/h2',xp_u: '//li[@class="card"]/a/@href',pre_u: 'https://www.hxm5.com',name: '线报屋-速度', list: 'hmd',int: "220"},
 { url: 'https://www.hxm5.com/s/bug%E4%BB%B7/', type: 'web', xp_t: '//li[@class="card"]/a/h2',xp_u: '//li[@class="card"]/a/@href',pre_u: 'https://www.hxm5.com',name: '线报屋-bug价', list: 'hmd',int: "220"},
 { url: 'https://new.xianbao.fun/plus/json/push.json', type: 'xianbao', name: '线报网-全部', list:'bmd' ,int:"100"},  
