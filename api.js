e923c3859ca6177b6f1912ff9a2e2d376b5952ca
leen2o <cnfoxy@gmail.com>
2025-09-26 15:09:41 +0800

    Update api.js

--- api.js
+++ api.js
@@ -10,4 +10,4 @@
 { url: 'https://new.xianbao.fun/plus/json/push_16.json', type: 'xianbao', name: '线报网-赚客吧', list:'bmd' ,int:"160"},
 { url: 'https://new.xianbao.fun/plus/json/push_18.json', type: 'xianbao', name: '线报网-新赚吧', list:'bmd' ,int:"180"},
 { url: 'https://new.xianbao.fun/plus/json/push_19.json', type: 'xianbao', name: '线报网-什么值得买', list:'bmd' ,int:"200"},
-{ url: 'https://new.xianbao.fun/plus/json/push_23.json', type: 'xianbao', name: '线报网-豆瓣线报', list:'bmd' ,int:"220"},
+{ url: 'https://new.xianbao.fun/plus/json/push_23.json', type: 'xianbao', name: '线报网-豆瓣线报', list:'bmd' ,int:"220"}, 
